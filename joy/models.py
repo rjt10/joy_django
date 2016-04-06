7f38e37d8e38d676955214458ef6b62a5b7c05ed
Jerry Tang <rj@petallabs.com>
2016-04-06 16:03:50 -0700

    Registered user model for admin and switched to PyCharm IDE.

--- joy/models.py
+++ joy/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class User(models.Model):
     joined = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100, blank=True, default='')
+    nickname = models.CharField(max_length=100, blank=True, default='')
+    fb_userid = models.CharField(max_length=100, blank=True, default='')
 
     class Meta:
         ordering = ('joined',)
