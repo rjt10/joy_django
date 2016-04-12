b58de15b9f094cc9ddb494c84ac9664c18f0006a
Jerry Tang <rj@petallabs.com>
2016-04-12 08:56:07 -0700

    Removed additional allowed_hosts and updated eb requirements.txt.

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,8 +28,6 @@ DEBUG = False
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    '.elasticbeanstalk.com',
-    '.elasticbeanstalk.com.',
     'joy-dev.us-west-2.elasticbeanstalk.com/',
 ]
 
